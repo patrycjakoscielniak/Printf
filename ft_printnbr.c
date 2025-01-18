bd44681457dff26c281a900e7c95ffaaa5d93cfc
Patrycja Kościelniak <126174009+patrycjakoscielniak@users.noreply.github.com>
2025-01-18 15:02:22 +0100

    Update and rename printnbr.c to ft_printnbr.c

--- ft_printnbr.c
+++ ft_printnbr.c
@@ -10,9 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
+
+static void	ft_putnbr(int nbr)
+{
+	if (nbr != 0)
+	{
+		ft_putnbr(nbr / 10);
+		ft_printchar(nbr % 10 + '0');
+	}
+	return ;
+}
+
+static int ft_countnbrs(int nbr)
+{
+	if (nbr != 0)
+		return 1 + ft_countnbrs(nbr / 10);
+	return (0);
+}
 
 int	ft_printnbr(int n)
 {
-	
+	int	count;
+
+	count = 0;
+	if (n < -2147483648 || n > 2147483647)
+		return (-1);
+	if (n == -2147483648)
+		return (ft_printstr("-2147483648"));
+	else if (n == 0)
+		return (ft_printchar('0'));
+	else if (n < 0)
+	{
+		count += ft_printchar('-');
+		n = -n;
+	}
+	ft_putnbr(n);
+	return (count + ft_countnbrs(n));
 }
